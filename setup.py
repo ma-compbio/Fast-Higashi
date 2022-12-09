b49a288d312b720b6ebc74dd2ff27e7decefb36b
ruochiz <ruochiz@andrew.cmu.edu>
2022-12-09 17:54:34 -0500

    test

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
-from setuptools import setup, find_packages
-print (find_packages())
-setup(
-    name='fast-higashi',
-    version='0.0.1a0',
-    description='Fast-Higashi: Ultrafast and interpretable single-cell 3D genome analysis',
-    url='https://github.com/ma-compbio/Fast-Higashi',
-    include_package_data=True,
-    python_requires='>=3.9',
-    packages=find_packages(),
-    install_requires=[
-        'numpy>=1.21.2',
-        'scipy==1.7.3',
-        'pandas==1.3.4',
-        'cython>=0.29.24',
-        'torch>=1.8.0',
-        'scikit-learn>=0.23.2',
-        'tqdm',
-        'h5py',
-        'seaborn>=0.11.2',
-        'umap-learn>=0.5',
-        'opt_einsum',
-        'pybedtools',
-        'pytorch',
-        'psutil'
-    ],
-    extras_require={},
-    author='Ruochi Zhang',
-    author_email='ruochiz@andrew.cmu.edu',
-    license='MIT'
-)
+# from setuptools import setup, find_packages
+# print (find_packages())
+# setup(
+#     name='fast-higashi',
+#     version='0.0.1a0',
+#     description='Fast-Higashi: Ultrafast and interpretable single-cell 3D genome analysis',
+#     url='https://github.com/ma-compbio/Fast-Higashi',
+#     include_package_data=True,
+#     python_requires='>=3.9',
+#     packages=find_packages(),
+#     install_requires=[
+#         'numpy>=1.21.2',
+#         'scipy==1.7.3',
+#         'pandas==1.3.4',
+#         'cython>=0.29.24',
+#         'torch>=1.8.0',
+#         'scikit-learn>=0.23.2',
+#         'tqdm',
+#         'h5py',
+#         'seaborn>=0.11.2',
+#         'umap-learn>=0.5',
+#         'opt_einsum',
+#         'pybedtools',
+#         'pytorch',
+#         'psutil'
+#     ],
+#     extras_require={},
+#     author='Ruochi Zhang',
+#     author_email='ruochiz@andrew.cmu.edu',
+#     license='MIT'
+# )
 
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="fasthigashi")
